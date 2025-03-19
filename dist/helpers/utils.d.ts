0614eff0f9a12ef50e1fd08ebc78de1371b3a8f2
Everson Neves <everson.neves@plantae.agr.br>
2025-03-19 08:50:34 -0400

    refactor: remove deprecated PlantaeFilter and related components from dist

--- dist/helpers/utils.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function debounce<T extends (...args: any[]) => void>(fn: T, delay?: number): (...args: Parameters<T>) => void;
--- dist/main.d.ts
+++
@@ -1,2 +0,0 @@
-import { PlantaeFilter } from './components/plantae-filter';
-export default PlantaeFilter;
